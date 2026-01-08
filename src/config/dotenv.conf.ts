c3370f9da3726c3ba9fcdc531d98d73824faa007
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-01-08 16:46:25 +0200

    feat: add Prisma setup with initial schema and configuration files

---
+++ src/config/dotenv.conf.ts
@@ -0,0 +1,8 @@
+import dotenv from "dotenv";
+import path from 'node:path'
+
+const envPath = process.env.NODE_ENV === 'development' ? '.env.dev' : '.env.production';
+
+export default dotenv.config({
+     path: path.resolve(process.cwd(), envPath)
+})
