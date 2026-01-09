335e55d7b049421e2d3f9f9509660621d23d4c1d
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-01-09 12:54:20 +0200

    feat: integrate Socket.IO for real-time communication and add Redis and Prisma configurations

---
+++ src/config/redis.ts
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+
+const client: any = createClient({
+     url: process.env.NODE_ENV == "development" ? process.env.REDIS_HOST_LOCALHOST : process.env.REDIS_HOST,
+     socket: {
+          connectTimeout: 30000,
+          reconnectStrategy: (retries) => Math.min(retries * 100, 3000),
+     },
+});
+
+client.connect().then(() => console.log(`🛢️  Redis connected successfully: ${process.env.NODE_ENV === "development" ? process.env.REDIS_HOST_LOCALHOST : process.env.REDIS_HOST}`));
+client.on("error", (err: any) => console.log("Redis Client Error", err));
+
+process.on('SIGINT', async () => {
+     await client.disconnect();
+     console.log('Redis connection closed');
+     process.exit(0);
+});
+export default client;
