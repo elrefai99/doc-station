5872579fd9f89353e4262c474762346ab95b0bc2
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-01-12 15:06:27 +0200

    feat: add login, logout, and refresh controllers to authentication module and update routing

--- src/modules/authentication/auth.controller.ts
+++ src/modules/authentication/auth.controller.ts
@@ -1,2 +1,4 @@
+export { refreshController } from "./Controller/refresh.controller";
+export { logoutController } from "./Controller/logout.controller";
+export { loginController } from "./Controller/login.controller";
 export { registerController } from "./Controller/register.controller";
-
