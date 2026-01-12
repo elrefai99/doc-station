48bcfadb9a27dfdbba4320fb23c8d47afb05f0fe
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-01-12 13:01:41 +0200

    feat: add class-validator dependency, update user registration logic to hash passwords, and modify Prisma schema to include password field

--- src/modules/authentication/Controller/register.controller.ts
+++ src/modules/authentication/Controller/register.controller.ts
@@ -1,29 +1,38 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { asyncHandler } from "../../../utils/asyncHandler.utils";
 import prisma from "../../../config/prisma";
 import ServerError from "../../../utils/api.errors.utils";
+import bcrypt from "bcrypt";
 
 export const registerController = asyncHandler(
-     async (req: Request, res: Response) => {
-          const { email, } = req.body;
+     async (req: Request, res: Response, next: NextFunction) => {
+          const { email, fullname, password } = req.body
+
           const user = await prisma.user.findFirst({
                where: {
                     email: email as string
                }
           })
           if (user) {
-               throw new ServerError("User already exists", 400);
+               next(new ServerError("User already exists", 409));
+               return
           }
 
-          const newUser = await prisma.user.create({
+          const salt = await bcrypt.genSalt(10);
+          const hashedPassword = await bcrypt.hash(password, salt);
+
+          const username = `${fullname.toLowerCase()}_${Math.floor(Math.random() * 1000)}`
+
+          await prisma.user.create({
                data: {
                     email,
-                    fullname: "dada",
-                    username: "dada",
-                    password: "dada",
+                    fullname,
+                    username,
+                    password: hashedPassword,
                }
           })
 
-          res.status(201).json({ user, newUser });
+          res.status(201).json({ code: 201, status: "Created", message: "User created successfully" });
+          return
      }
 )
