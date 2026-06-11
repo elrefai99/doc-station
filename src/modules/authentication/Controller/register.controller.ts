4985d603327c4ae91e1e8c836ec2d0b8b76e02d3
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-06-11 21:55:32 +0300

    feat: implement avatar generation and user authentication service

--- src/modules/authentication/Controller/register.controller.ts
+++ src/modules/authentication/Controller/register.controller.ts
@@ -5,7 +5,7 @@ import ServerError from "../../../utils/api.errors.utils";
 import bcrypt from "bcrypt";
 import { pending_token } from "../../../utils/JWT/pending.account.jwt";
 import { registerDto } from "../DTO/index.dto";
-import { avatarProfile } from "../shared/avatar.image.fun";
+import { avatarProfile } from "../utils/avatar.image.fun";
 import { UserRole, UserStatus } from "../../../generated/prisma";
 
 export const registerController = asyncHandler(
---
+++ src/modules/authentication/service/base.auth.service.ts
@@ -0,0 +1,20 @@
+
+export class baseAuthService {
+     constructor() { }
+
+     public async check_account() {
+          return "register"
+     }
+
+     public async register() {
+          return "register"
+     }
+
+     async login() {
+          return "login"
+     }
+
+     async refreshToken() {
+
+     }
+}
